6fe29d4d86fb00ab3fbb27e66fbece78aa442999
jwalk99star <jwalk99star@gmail.com>
2023-04-25 17:14:12 -0600

    lesson 44 completed

--- main.js
+++ main.js
@@ -59,13 +59,16 @@ function draw(){
   let clickCountElem = document.getElementById("click-count")
   let popCountElem = document.getElementById('pop-count')
   let highPopCountElem = document.getElementById('high-pop-count')
-
+  let playerNameElem = document.getElementById('player-name')
+  
   balloonElement.style.height = height + "px"
   balloonElement.style.width = width + "px"
   
   clickCountElem.innerText = clickCount.toString()
   popCountElem.innerText = currentPopCount.toString()
   highPopCountElem.innerText = currentPlayer.topScore.toString()
+
+  playerNameElem.innerText = currentPlayer.name
 }
 
 function stopGame(){
@@ -118,7 +121,6 @@ function changePlayer(){
   document.getElementById("player-form").classList.remove("hidden")
   document.getElementById("game").classList.add("hidden")
 }
-
 
 function savePlayers(){
   window.localStorage.setItem("players", JSON.stringify(players))
